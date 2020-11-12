988f3758140c51cd24bdd79dffdf9a7999d0172d
Buttercuper777 <71713443+Buttercuper777@users.noreply.github.com>
2020-11-12 15:41:15 +0300

    Starting calc

--- SITE/JS/main_script.js
+++ SITE/JS/main_script.js
@@ -1,34 +1,70 @@
+jQuery(function($){
+    $("input[type='phone']").mask("+7 (999) 999-99-99");
+ });
+
+  function myFunction(element) {
+    var $temp = $("<input>");
+    $("body").append($temp);
+    $temp.val($(element).text()).select();
+    document.execCommand("copy");
+    $temp.remove();
+  }
+
+
 $(document).ready(function() {
-    $("#CalcBtn").click(function (e) { 
-        $('#CalcBtn, #ChooseForm').toggleClass('active');
-        $('.headerNavigationBlock').css('transition', '0.5s ease');
-        $('.headerNavigationBlock').css('opacity', '0.3');
-
-        $('.leftContent').css('transition', '0.5s ease');
-        $('.leftContent').css('opacity', '0.3');
-
-        $('.buttonBtm').css('transition', '0.5s ease');
-        $('.buttonBtm').css('opacity', '0.3');
-
-        $('#BackWall').css('transform', 'scale(1)');
-
-        $('.ImageCont').css('opacity', '1');
+   
+    $("#CalcBtn, #headerNavLinkCalc").click(function (e) { 
+        $('.headerNavigationBlock, #ChooseForm, .leftContent, .buttonBtm, #BackWall').addClass('active');
     });
 
-    $("#BackWall").click(function (e) { 
-        $('#CalcBtn, #ChooseForm').toggleClass('active');
-
-        $('.headerNavigationBlock').css('opacity', '1');
-        $('.headerNavigationBlock').css('transition', '0');
-
-        $('.leftContent').css('opacity', '1');
-        $('.leftContent').css('transition', '0');
-
-        $('.buttonBtm').css('opacity', '1');
-        $('.buttonBtm').css('transition', '0');
-
-        $('#BackWall').css('transform', 'scale(0)');
-
-        $('.ImageCont').css('opacity', '1');
+    $("#BackWall, .close_svg").click(function (e) { 
+        $('.headerNavigationBlock, #ChooseForm, .leftContent, .buttonBtm, #BackWall').removeClass('active')
+        $('#BackWall, .popup, .PlacePop').removeClass('CallPop');
+        $('.chooseBlock').removeClass('active');
+        $('.aboutBlock').removeClass('active');      
     });
+
+    $("#CallBtn, #call_svg").click(function (e) { 
+        $('#BackWall, .popup').addClass('CallPop'); 
+        $('.aboutBlock').removeClass('active');     
+    });
+
+    $(".headerPlaceBtn").click(function (e) { 
+        $('#BackWall, .PlacePop').addClass('CallPop'); 
+        $('.chooseBlock').addClass('active');
+        $('.PlacePop').addClass('active');       
+    });
+
+    $(".aboutPlace").click(function (e) { 
+        $('.chooseBlock').addClass('active');
+        $('.chooseBlock').removeClass('active');
+        $('.PlacePop').removeClass('active'); 
+        $('.aboutBlock').addClass('active');   
+    });
+
+    
+    $(".back_click").click(function (e) { 
+        $('.aboutBlock').removeClass('active'); 
+        $('.chooseBlock').addClass('active');
+        $('.PlacePop').addClass('active'); 
+    });
+
+    $(".ico_img_about").mouseenter(function (e) {
+        if(this.id == 'mail_svg')
+            $('#about_choosed_type').text("Отправить email");
+        if(this.id == 'call_svg')
+            $('#about_choosed_type').text("Заказать звонок");
+        if(this.id == 'phone_svg')
+            $('#about_choosed_type').text("Позвонить нам");
+    });
+
+    $("#phone_svg").click(function (e) { 
+        alert("+7 (918) 982-91-53");
+    });
+
+    $("#mail_svg").click(function (e) { 
+        alert("steklopro23@mail.ru \nАдрес скопирован в буфер обмена");
+    });
+
+
 });
